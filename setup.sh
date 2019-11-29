013e465e9a12f5763b87076c05a0ff46ae1ae30b
grassknoted <grassknoted@gmail.com>
2019-11-30 02:31:19 +0530

    windows setup added

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ pip install -r requirements.txt
 cd pyflow
 python setup.py build_ext -ID
 cd ..
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1vZm3LDdJLwBP-1LNVCV9oP8a77lRUjvp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1vZm3LDdJLwBP-1LNVCV9oP8a77lRUjvp" -O handwash_step_model.h5
+rm -rf /tmp/cookies.txt
---
+++ windows_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Setup Script for the Sample Code Submission of Paper ID: 9202
+
+# Install the required Python Modules
+pip install -r requirements.txt
+
+# PyFlow for Windows
+sed -i '9s/.*//' pyflow/src/project.h
+
+# Setup PyFlow for Optical Flow Computation
+cd pyflow
+python setup.py build_ext -ID
+cd ..
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1vZm3LDdJLwBP-1LNVCV9oP8a77lRUjvp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1vZm3LDdJLwBP-1LNVCV9oP8a77lRUjvp" -O handwash_step_model.h5 && rm -rf /tmp/cookies.txt
